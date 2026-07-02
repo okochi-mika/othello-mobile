48827322ab2b9570fe0f07c8d91670ba7e81bfcb
okochi-mika <hiyoko_m@icloud.com>
2026-07-02 20:40:40 +0900

    スマホ版オセロゲーム(Capacitor/iOS)初回コミット

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.othellogame',
+  appName: 'オセロ風ゲーム',
+  webDir: 'www'
+};
+
+export default config;
